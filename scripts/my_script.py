211592ae36dbd92471325b312b0ebcd06b853e8f
Yixing Wang <yxonestarw@gmail.com>
2019-06-07 00:37:39 -0700

    Set up script for the first time

--- scripts/my_script.py
+++ scripts/my_script.py
@@ -2,15 +2,19 @@
 
 # This adds the directory above to our Python path
 #   This is so that we can add import our custom python module code into this script
-import sys
+import os, sys
 sys.path.append('../')
+
+import pygame
+from pygame.locals import *
+
+if not pygame.font:
+    print('Warning, fonts disabled')
+if not pygame.mixer:
+    print('Warning, sound disabled')
 
 # Imports
 from my_module.functions import my_func, my_other_func
 
-###
-###
-
-# PYTHON SCRIPT HERE
 
 pass
