cc6d5d481f086a7e9aa92793c9ec3d77feb5db5b
Yixing Wang <yxonestarw@gmail.com>
2019-06-10 23:36:27 -0700

    Enabled infinite stairs

--- my_module/Stair.py
+++ my_module/Stair.py
@@ -2,17 +2,19 @@ import pygame
 from my_module.main_screen_funcs import load_image
 
 class Stair(pygame.sprite.Sprite):
-    def __init__(self, image, speed = 10):
+    current_stairs = 0
+    def __init__(self, image, pos, screen, speed = 10):
         pygame.sprite.Sprite.__init__(self)
         self.image = load_image(image)
+        self.area = screen.get_rect()
         self.speed = speed
-        self.rect = self.image.get_rect().move(0, 750)
-
-    def renderImage(self):
-        return self.image
-
-    def getPos(self):
-        return self.rect
+        self.rect = self.image.get_rect().move(pos[0], pos[1])
 
     def update(self):
         self.rect = self.rect.move(0, -self.speed)
+        if self.rect.bottom < self.area.top:
+            self.kill()
+            Stair.current_stairs -= 1
+
+    def getPos(self):
+        return (self.rect.left, self.rect.top)
--- scripts/my_script.py
+++ scripts/my_script.py
@@ -1,7 +1,7 @@
 """Script to run some part of my project."""
 
 # Imports
-import sys
+import sys, random
 # This adds the directory above to our Python path
 # so that we can add import our custom python module code into this script
 sys.path.append('../')
@@ -20,17 +20,22 @@ from my_module.Stair import Stair
 # Constants we will use for screen
 size = 1200, 750
 black = 0, 0, 0
-
+max_stairs = 10
 
 
 # Set up the main screen
 screen = pygame.display.set_mode(size)
 
 start_speed = 10
-stair = Stair('stair.png', start_speed)
+pos0 = 100, 750
 
 stairs = pygame.sprite.Group()
-stairs.add(stair)
+lowest_stair = pygame.sprite.GroupSingle()
+
+new_stair = Stair('stair.png', pos0, screen, start_speed)
+stairs.add(new_stair)
+lowest_stair.add(new_stair)
+Stair.current_stairs = 1
 
 # Loop of animation and all the program this game needs
 while True:
@@ -38,9 +43,18 @@ while True:
         if event.type in (QUIT, KEYDOWN):
             sys.exit()
 
+    if Stair.current_stairs < max_stairs:
+        last_pos = lowest_stair.sprite.getPos()
+        new_pos = (last_pos[0] + 30, last_pos[1] + 200)
+        new_stair = Stair('stair.png', new_pos, screen, start_speed)
+        stairs.add(new_stair)
+        lowest_stair.add(new_stair)
+        Stair.current_stairs += 1
+        print(Stair.current_stairs)
+
     stairs.update()
+    
 
     screen.fill(black)
-    #screen.blit(stair.renderImage(), stair.getPos())
     stairs.draw(screen)
     pygame.display.flip()
